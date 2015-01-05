3a426c1d87562bb4aee63c3d37720bfc62b4e730
Katrin Kerber <katrinkerber@gmail.com>
2015-01-05 16:38:27 +0000

    added function for replicator sets

--- ckeditor/hooks.ckeditor.php
+++ ckeditor/hooks.ckeditor.php
@@ -12,9 +12,9 @@ class Hooks_ckeditor extends Hooks {
   public function control_panel__add_to_foot()
   {
       if (URL::getCurrent(false) == '/publish') {
-        $jsPath = Config::getSiteRoot()."_add-ons/ckeditor/ckeditor.js"; 
+        $jsPath = Config::getSiteRoot()."_add-ons/ckeditor/ckeditor.js";
         $html = '<script src="' . $jsPath . '"></script>';
-        
+
         $html .= "<script>
           function initiateCKEditors() {
             $('.ckeditor').each(function(){
@@ -23,11 +23,11 @@ class Hooks_ckeditor extends Hooks {
           }
 
           // initiate CKEditor for field(s) in a newly added grid row
-          function newRowEditor() {    
+          function newRowEditor() {
 
             // get the right grid field for the clicked 'add row' button
             var thisGridTable = $(this).siblings('table.grid');
-            
+
             setTimeout(function() {
               // select ckeditor cell in new row
               var newRowCell = thisGridTable.find('tbody tr:last td.cell-ckeditor');
@@ -43,15 +43,38 @@ class Hooks_ckeditor extends Hooks {
 
           }
 
+          // initiate CKEditor field(s) in replicator sets
+          function newReplicatorEditor() {
+
+            var thisReplicatorParent = $(this).parents('.input-replicator');
+
+            setTimeout(function() {
+
+              thisReplicatorParent.find('.replicator-set').each(function(){
+                var thisCkeditorFieldContainer = $(this).find('.ckeditor-container');
+
+                if ( !thisCkeditorFieldContainer.find('div.cke').length ) {
+                  thisCkeditorFieldContainer.find('textarea.ckeditor').each(function(){
+                    CKEDITOR.replace( $(this).attr('name') );
+                  });
+                }
+              });
+
+            }, 50);
+
+          }
+
           $(document).ready(function() {
 
             initiateCKEditors();
 
             $('.grid-add-row').click(newRowEditor);
 
+            $('.replicator-buttons button').click(newReplicatorEditor);
+
           });
         </script>";
-        
+
         return $html;
       }
   }
